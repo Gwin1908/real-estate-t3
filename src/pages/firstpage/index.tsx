1e89c64d4abfb9de402b35a1d1d380a1faf8ad28
Kovtun Taras <taras.kovtun.2017@gmail.com>
2023-10-06 14:28:36 +0300

    adding home page

---
+++ src/pages/firstpage/index.tsx
@@ -0,0 +1,51 @@
+import styles from "../../styles/FirstPage.module.scss";
+
+function FirstPage() {
+  return (
+    <section className={styles.firstPage}>
+      <div className={styles.title}>
+        <h1 className={styles.title__header}>Agency Name</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
+          voluptatibus?
+        </p>
+        <div className={styles.keyFrases}>
+          <div className={styles.keyFrase}>Key Frase</div>
+          <div className={styles.keyFrase}>Key Frase</div>
+          <div className={styles.keyFrase}>Key Frase</div>
+        </div>
+      </div>
+
+      <svg
+        id="scroll-me"
+        className={styles.scrollMe}
+        data-anchor="properties"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 76 130"
+        preserveAspectRatio="xMidYMid meet"
+        // tabIndex="-1"
+      >
+        <g fill="none" fill-rule="evenodd">
+          <rect
+            width="70"
+            height="118"
+            x="1.5"
+            y="1.5"
+            stroke="#FFF"
+            stroke-width="3"
+            rx="36"
+          ></rect>
+          <circle
+            className={styles.scroll}
+            cx="36.5"
+            cy="31.5"
+            r="7"
+            fill="#FFF"
+          ></circle>
+        </g>
+      </svg>
+    </section>
+  );
+}
+
+export default FirstPage;
