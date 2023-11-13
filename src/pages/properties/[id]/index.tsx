79752d796960eb0d2948d02ad87c8986200a43c8
Kovtun Taras <taras.kovtun.2017@gmail.com>
2023-11-14 00:09:44 +0200

    vercel check

--- src/pages/properties/[id]/index.tsx
+++ src/pages/properties/[id]/index.tsx
@@ -3,25 +3,26 @@ import SingleProperty from "~/components/SingleProperty/SingleProperty";
 import { api } from "~/utils/api";
 
 function PropertyById() {
-  const router = useRouter();
+  // const router = useRouter();
 
-  const propertyId = router.query.id?.toString() ?? ""
+  // const propertyId = router.query.id?.toString() ?? ""
 
-  console.log(propertyId)
+  // console.log(propertyId)
 
-  const { data } = api.property.getPropertyById.useQuery(propertyId);
+  // const { data } = api.property.getPropertyById.useQuery(propertyId);
 
   return (
-    !!data && (
-      <SingleProperty
-        id={data.id}
-        images={data.images}
-        name={data.name}
-        description={data.description}
-        address={data.address}
-        price={data.price}
-      />
-    )
+    <h1>Loading</h1>
+    // !!data && (
+    //   <SingleProperty
+    //     id={data.id}
+    //     images={data.images}
+    //     name={data.name}
+    //     description={data.description}
+    //     address={data.address}
+    //     price={data.price}
+    //   />
+    // )
   );
 }
 
